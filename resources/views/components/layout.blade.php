08a1903764a901053e5d986b5faff22d2bc258db
Samuele Di Cori <https://github.com/Hackademy-PT-9/Samuele-ProgettoFinale.git>
2023-12-07 16:56:45 +0100

    Scaffoldin front-end realizzato

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{$title ?? 'Presto.it'}}</title>
+
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    {{$style ?? ''}}
+</head>
+<body>
+    
+    <x-nav />
+
+    <div class="min-vh-100">
+        {{$slot}}
+    </div>
+
+    <x-footer />
+
+    <script src="{{asset('js/app.js')}}"></script>
+    {{$script ?? ''}}
+
+</body>
+</html>
